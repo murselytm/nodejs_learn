80108f45194df542cd3fb3c378b9f35c0ace94a9
murselytm <61565081+murselytm@users.noreply.github.com>
2023-02-08 16:11:17 +0300

    Create hw2.js

---
+++ nodeJS_Nedir/hw2.js
@@ -0,0 +1,29 @@
+const posts = [
+  {name: "Post1"},
+  {name: "Post2"}
+];
+
+const listPost = () => {
+  posts.map((post) => {
+  console.log(posts.name);  
+  });
+};
+
+const addPost = (newPost) => {
+  const promise1 = new Promise((resolve, reject) => {
+    posts.push(newPost);
+    resolve(posts);
+  });
+  return promise1;
+};
+
+async function showPosts(){
+  try{
+    await addPost({ name:"Post3"});
+    listPost();
+  } catch(error){
+    console.log(error);
+  }
+}
+
+showPosts();
