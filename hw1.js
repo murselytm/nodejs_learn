356a60bdfe4f2f2d26576e82c42f51f28d710df7
murselytm <61565081+murselytm@users.noreply.github.com>
2023-02-08 11:42:31 +0300

    Delete hw1.js

--- hw1.js
+++
@@ -1,8 +0,0 @@
-const arguments = process.argv.slice(2)
-
-function alanHesapla(yaricap) {
-    let alan = 3.14 * yaricap * yaricap
-    console.log('yaricapi : ' + yaricap + ' olan dairenin alani: ' + alan)
-}
-
-alanHesapla(5)
